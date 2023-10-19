3ee5e0899f1aa07affb2a7df49fa8eba98442ea2
jefferson-moraiis <jefferson.orlando.morais@hotmail.com>
2023-10-19 02:38:46 -0300

    fix; currence in value totalPrice

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -1,6 +1,6 @@
 import { Product } from '@prisma/client';
 
-interface ProductWithTotalPrice extends Product {
+export interface ProductWithTotalPrice extends Product {
   totalPrice: number;
 }
 
@@ -12,11 +12,11 @@ export const computeProductTotalPrice = (product:Product):ProductWithTotalPrice 
     }
   }
 
-  const totalPrice = (Number(product.basePrice) * Number(product.discountPercentage) / 100);
+  const totalDiscount = (Number(product.basePrice) * Number(product.discountPercentage) / 100);
 
   return {
     ...product,
-    totalPrice
+    totalPrice: Number(product.basePrice) - totalDiscount
   }
 }
  
